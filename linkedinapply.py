8b02c910490ba875675bbf1d5f7f39dbdba1111d
eranceylan <eranceylan@gmail.com>
2017-06-19 13:18:14 +0300

    Add files via upload

--- linkedinapply.py
+++ linkedinapply.py
@@ -8,6 +8,14 @@ from getpass import getpass
 from lxml import html
 from sys import exit
 from os import path
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
 
 # reference globals
 ROOT_URL =          "https://www.linkedin.com"
